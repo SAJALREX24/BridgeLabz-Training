e5be92b2c88ce8a06ed111b5d5ba7502684bf03b
SAREXER <sajalsaraswat12@gmail.com>
2025-12-19 12:47:57 +0530

    Practice of C# TOPICS OPERATORS AND DATATYPES

---
+++ core-csharp-practice/gcr-codebase/programming-elements/datatypess.cs
@@ -0,0 +1,53 @@
+using System;
+
+class Random{
+	static void Main(String[] args){
+		
+		Console.WriteLine("Primitive Datatypes");
+		
+		
+		
+		Console.WriteLine("int Datatype");
+		int number=24;
+		Console.WriteLine(number);
+		
+		
+		Console.WriteLine("float Datatype ");
+		float percentage=24.4f;
+		Console.WriteLine(percentage);
+		
+		Console.WriteLine("double Datatype");
+		double decimall=24.249024;
+		Console.WriteLine(decimall);
+		
+		Console.WriteLine("char Datatype ");
+		char letters='S';
+		Console.WriteLine(letters);
+		
+		
+		
+		Console.WriteLine("bool Datatype ");
+		bool Ben=true;
+		Console.WriteLine(Ben);
+		
+		
+		
+		Console.WriteLine("Type Conversions");
+		
+		
+		
+		Console.WriteLine("Implicit Conversion value : 24");
+		int First=24;
+		double Second=First;
+		Console.WriteLine(Second);
+		
+		Console.WriteLine("Explicit Conversion value : 24.24");
+		double Third=24.24;
+		int Forth=(int)Third;
+		Console.WriteLine(Forth);
+		
+		
+		
+		
+	} 
+}
